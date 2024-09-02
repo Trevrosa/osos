0cf3854773ac4f2cab8204034f18a73e857c5c53
trev <trev@trevrosa.dev>
2024-09-02 16:40:18 +0800

    Make interrupt handlers their own module

---
+++ src/interrupts/keyboard.rs
@@ -0,0 +1,35 @@
+use pc_keyboard::{layouts::Us104Key, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+use spin::Mutex;
+use x86_64::{instructions::port::Port, structures::idt::InterruptStackFrame};
+use lazy_static::lazy_static;
+
+use crate::{interrupts::{notify_end_of_interrupt, InterruptIndex}, print, serial_println};
+
+pub extern "x86-interrupt" fn handler(_frame: InterruptStackFrame) {
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<Us104Key, ScancodeSet1>> = {
+            Mutex::new(Keyboard::new(
+                ScancodeSet1::new(),
+                Us104Key,
+                HandleControl::Ignore,
+            ))
+        };
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    let scancode: u8 = unsafe { Port::new(0x60).read() };
+
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                // not char keys
+                DecodedKey::RawKey(key) => serial_println!("{key:?}"),
+            };
+        }
+    }
+
+    unsafe {
+        notify_end_of_interrupt(InterruptIndex::Keyboard as u8);
+    }
+}
---
+++ src/interrupts/timer.rs
@@ -0,0 +1,11 @@
+use x86_64::structures::idt::InterruptStackFrame;
+
+use crate::{interrupts::{notify_end_of_interrupt, InterruptIndex}, print};
+
+pub extern "x86-interrupt" fn handler(_frame: InterruptStackFrame) {
+    print!(".");
+
+    unsafe {
+        notify_end_of_interrupt(InterruptIndex::Timer as u8);
+    }
+}
